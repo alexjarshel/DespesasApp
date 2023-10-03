5a9ae573e432ff61ba9e21bf42f836bb094a71c1
unknown <alexbaukartjarschel@gmail.com>
2023-10-03 20:35:51 -0300

    .

---
+++ lib/Pages/my_home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('despesas Pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('grafico'),
+              ),
+            ),
+            Card(
+              child: Text('listas de transções'),
+            )
+          ],
+        ));
+  }
+}
